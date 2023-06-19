e67e9cbe86ad9ead89d97565c085a24f41d48659
Felipe Fransa <antoniofelipesilva@live.com>
2023-06-19 03:44:54 -0300

    add new main/about

--- src/components/aboutMe.tsx
+++ src/components/aboutMe.tsx
@@ -6,21 +6,22 @@ import iconTS from "../../assets/typescript.png"
 
 export const About = () => {
     return(
-        <div className="flex flex-row w-full h-96 justify-center items-center rounded-md ">
-            <div className="flex flex-col justify-center w-1/5 h-96">
-                <img src={iconHTML.src} alt="htlm" className="w-40 h-40 hover:border-2 border-rose-500 rounded-md hover:cursor-pointer" />
+        <div className="flex flex-row w-full h-96 justify-around items-center rounded-lg">
+            
+            <div className="flex flex-col justify-center items-center w-1/5 h-96 bg-gradient-to-r from-white to-red-500">
+                <img src={iconHTML.src} alt="htlm" className="w-40 h-40 hover:w-44 hover:h-44 hover:cursor-pointer" />
             </div>
-            <div className="flex flex-col justify-center w-1/5 h-96">
-                <img src={iconCss.src} alt="css" className="w-40 h-40 hover:border-2 border-rose-500 rounded-md hover:cursor-pointer" />
+            <div className="flex flex-col justify-center items-center  w-1/5 h-96 bg-gradient-to-r from-red-500 to-blue-500">
+                <img src={iconCss.src} alt="css" className="w-40 h-40 hover:w-44 hover:h-44 hover:cursor-pointer" />
             </div>
-            <div className="flex flex-col justify-center w-1/5 h-96">
-                <img src={iconJS.src} alt="JavaScript" className="w-40 h-40 hover:border-2 border-rose-500 rounded-md hover:cursor-pointer" />
+            <div className="flex flex-col justify-center items-center  w-1/5 h-96 bg-gradient-to-r from-blue-500 to-yellow-400">
+                <img src={iconJS.src} alt="JavaScript" className="w-40 h-40 hover:w-44 hover:h-44 hover:cursor-pointer" />
             </div>
-            <div className="flex flex-col justify-center w-1/5 h-96">
-                <img src={iconTS.src} alt="typeScript" className="w-40 h-40 hover:border-2 border-rose-500 rounded-md hover:cursor-pointer" />
+            <div className="flex flex-col justify-center items-center  w-1/5 h-96 bg-gradient-to-r from-yellow-400 to-green-400">
+                <img src={iconTS.src} alt="typeScript" className="w-40 h-40 hover:w-44 hover:h-44 hover:cursor-pointer" />
             </div>
-            <div className="flex flex-col justify-center w-1/5 h-96">
-                <img src={iconNode.src} alt="node" className="w-40 h-40 hover:border-2 border-rose-500 rounded-md hover:cursor-pointer" />
+            <div className="flex flex-col justify-center items-center  w-1/5 h-96 bg-gradient-to-r from-green-400 to-white">
+                <img src={iconNode.src} alt="node" className="w-40 h-40 hover:w-44 hover:h-44 hover:cursor-pointer" />
             </div>
         </div>
     )
--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,14 +1,25 @@
 import Avatar from "../../assets/felipe.jpg"
+import iconWhatsapp from "../../assets/icos/whatsapp.svg";
+import iconLinkedin from "../../assets/icos/linkedin.svg";
+import iconInstagram from "../../assets/icos/instagram.svg";
 
 export const Main = () => {
     return(
-        <div className="flex flex-row justify-around border-2 border-rose-500 w-full h-96 hover:bg-gradient-to-r from-cyan-500 to-blue-500 hover:font-bold hover:duration-150 items-center rounded-md ">
+        <div className="flex flex-row justify-around w-full h-96 items-center rounded-md ">
             <div className="flex flex-col justify-center text-center w-2/4">
-                <h2 className="text-9xl">Developer</h2>
-                <h3 className="text-8xl">JavaScript</h3>
+                <h2 className="text-9xl hover:cursor-pointer hover:font-bold">Developer</h2>
+                <h3 className="text-8xl hover:cursor-pointer hover:font-bold">JavaScript</h3>
             </div>
-            <div className="flex  justify-center w-2/4">
-                <img src={Avatar.src} alt="Felipe" className="w-72 h-72 border-2 border-rose-500 rounded-full" />
+            <div className="flex flex-col justify-center w-2/4 h-full">
+
+                <div className="flex items-center justify-center w-full h-3/4">
+                    <img src={Avatar.src} alt="Felipe" className="w-64 h-64 hover:border-2 border-rose-500 rounded-full" />
+                </div>
+                <div className="flex flex-row justify-center items-center w-full h-1/4">
+                    <img src={iconInstagram.src} alt="instagram" className="w-10 h-10 ml-4 px-2 rounded-lg bg-rose-600 hover:w-12 hover:h-12 hover:cursor-pointer" />
+                    <img src={iconLinkedin.src} alt="linkedin" className="w-10 h-10 ml-4 px-2 rounded-lg bg-blue-700 hover:w-12 hover:h-12 hover:cursor-pointer"/>
+                    <img src={iconWhatsapp.src} alt="whatsapp" className="w-10 h-10 ml-4 px-2 rounded-lg bg-green-600 hover:w-12 hover:h-12 hover:cursor-pointer"/>
+                </div>
             </div>
         </div>
     )
