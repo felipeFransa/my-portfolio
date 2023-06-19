981e4f3dc44d11410aec7a2530d58e303cdfad04
Felipe Fransa <antoniofelipesilva@live.com>
2023-06-18 23:08:23 -0300

    add About 2.0

--- src/components/aboutMe.tsx
+++ src/components/aboutMe.tsx
@@ -1,22 +1,26 @@
-// import Avatar from "../../assets/"
+import iconCss from "../../assets/css.png"
+import iconHTML from "../../assets/html.png"
+import iconJS from "../../assets/javascript.png"
+import iconNode from "../../assets/node.png"
+import iconTS from "../../assets/typescript.png"
 
 export const About = () => {
     return(
-        <div className="flex flex-row border-2 border-rose-500 w-full h-96 hover:bg-gradient-to-r from-cyan-500 to-blue-500 hover:font-bold hover:duration-150 justify-center items-center rounded-md ">
-            <div className="flex flex-col border-2 border-rose-500 w-1/5 h-80">
-                <img src="" alt="" />
+        <div className="flex flex-row w-full h-96 justify-center items-center rounded-md ">
+            <div className="flex flex-col justify-center w-1/5 h-96">
+                <img src={iconHTML.src} alt="htlm" className="w-40 h-40 hover:border-2 border-rose-500 rounded-md hover:cursor-pointer" />
             </div>
-            <div className="flex flex-col border-2 border-rose-500 w-1/5 h-80">
-                <img src="" alt="" />
+            <div className="flex flex-col justify-center w-1/5 h-96">
+                <img src={iconCss.src} alt="css" className="w-40 h-40 hover:border-2 border-rose-500 rounded-md hover:cursor-pointer" />
             </div>
-            <div className="flex flex-col border-2 border-rose-500 w-1/5 h-80">
-                <img src="" alt="" />
+            <div className="flex flex-col justify-center w-1/5 h-96">
+                <img src={iconJS.src} alt="JavaScript" className="w-40 h-40 hover:border-2 border-rose-500 rounded-md hover:cursor-pointer" />
             </div>
-            <div className="flex flex-col border-2 border-rose-500 w-1/5 h-80">
-                <img src="" alt="" />
+            <div className="flex flex-col justify-center w-1/5 h-96">
+                <img src={iconTS.src} alt="typeScript" className="w-40 h-40 hover:border-2 border-rose-500 rounded-md hover:cursor-pointer" />
             </div>
-            <div className="flex flex-col border-2 border-rose-500 w-1/5 h-80">
-                <img src="" alt="" />
+            <div className="flex flex-col justify-center w-1/5 h-96">
+                <img src={iconNode.src} alt="node" className="w-40 h-40 hover:border-2 border-rose-500 rounded-md hover:cursor-pointer" />
             </div>
         </div>
     )
--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,9 +1,14 @@
+import Avatar from "../../assets/felipe.jpg"
+
 export const Main = () => {
     return(
-        <div className="flex border-2 border-rose-500 w-full h-96 hover:bg-gradient-to-r from-cyan-500 to-blue-500 hover:font-bold hover:duration-150 justify-center items-center rounded-md ">
-            <div>
+        <div className="flex flex-row justify-around border-2 border-rose-500 w-full h-96 hover:bg-gradient-to-r from-cyan-500 to-blue-500 hover:font-bold hover:duration-150 items-center rounded-md ">
+            <div className="flex flex-col justify-center text-center w-2/4">
                 <h2 className="text-9xl">Developer</h2>
                 <h3 className="text-8xl">JavaScript</h3>
+            </div>
+            <div className="flex  justify-center w-2/4">
+                <img src={Avatar.src} alt="Felipe" className="w-72 h-72 border-2 border-rose-500 rounded-full" />
             </div>
         </div>
     )
