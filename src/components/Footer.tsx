7ccbd5f7b9d78cfa05f2fbc4e1e0e66422fdc8e1
Felipe Fransa <antoniofelipesilva@live.com>
2023-07-13 17:50:42 -0300

    version.1.0.0

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,14 +9,16 @@ export const Footer = () => {
         <div className="flex flex-col w-full h-full justify-around items-center">
 
             <div className="w-full h-36 flex flex-row justify-between items-center">
-                <div>
-                    <h2>Links</h2>
+                <div className="w-2/6 h-full flex flex-col justify-center items-start">
+                    <h2>Contato</h2>
+                    {/* <h4>email: felipefransa@exemplo.com</h4>
+                    <h4>Telefone: 31-971228890</h4> */}
                 </div>
-                <div>
-                    <h2>Sociais</h2>
+                <div className="w-2/6 h-full flex flex-col justify-center items-center">
+                    <h2>Redes Sociais</h2>
                 </div>
-                <div>
-                    <h2>About</h2>
+                <div className="w-2/6 h-full flex flex-col justify-center items-end">
+                    <h2>Sobre</h2>
                 </div>
             </div>
 
@@ -24,24 +26,24 @@ export const Footer = () => {
 
                 <div className="w-full h-12 flex justify-center items-center pb-3 my-2">
 
-                    <div className="w-12 h-12 flex justify-center items-center bg-white rounded-xl mx-2">
+                    <div className="w-12 h-12 flex justify-center items-center bg-white rounded-xl mx-2 hover:cursor-pointer">
                         <img src={Whatsapp.src} alt="" className="w-10 h-10"/>
                     </div>
-                    <div className="w-12 h-12 flex justify-center items-center bg-white rounded-xl mx-2">
+                    <div className="w-12 h-12 flex justify-center items-center bg-white rounded-xl mx-2 hover:cursor-pointer">
                         <img src={Linkedin.src} alt="" className="w-10 h-10"/>
                     </div>
-                    <div className="w-12 h-12 flex justify-center items-center bg-white rounded-xl mx-2">
+                    <div className="w-12 h-12 flex justify-center items-center bg-white rounded-xl mx-2 hover:cursor-pointer">
                         <img src={Instagram.src} alt="" className="w-10 h-10"/>
                     </div>
-                    <div className="w-12 h-12 flex justify-center items-center bg-white rounded-xl mx-2">
+                    <div className="w-12 h-12 flex justify-center items-center bg-white rounded-xl mx-2 hover:cursor-pointer">
                         <img src={Facebook.src} alt="" className="w-10 h-10"/>
                     </div>
-                    <div className="w-12 h-12 flex justify-center items-center bg-white rounded-xl mx-2">
+                    <div className="w-12 h-12 flex justify-center items-center bg-white rounded-xl mx-2 hover:cursor-pointer">
                         <img src={Twitter.src} alt="" className="w-10 h-10"/>
                     </div>
                 </div>
                
-                <div>
+                <div className="w-full flex flex-row justify-center items-center">
                     <h3>
                         @Copyright Felipe Developer
                     </h3>
